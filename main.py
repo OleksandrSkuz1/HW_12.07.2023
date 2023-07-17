966c8b80581f4ae93646178f21985971f95b2a18
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-17 14:55:43 +0300

    add

--- main.py
+++ main.py
@@ -2,28 +2,10 @@ if __name__ == '__main__':
     start = int(input("Введіть початкове число діапазону: "))
     end = int(input("Введіть кінцеве число діапазону: "))
 
-    def is_prime(number):
+    numbers = [start, end]
+    i = 2
+    for i in range(start, end + 1):
+        if i > 2:
+         elif i % i == 0:
 
-        if number < 2:
-            return False
-
-        for i in range(2, int(number ** 0.5) + 1):
-
-            if number % i == 0:
-                return False
-
-        return True
-
-    def find_primes(start, end):
-
-        primes = []
-
-        for number in range(start, end + 1):
-
-            if is_prime(number):
-                primes.append(number)
-
-        return primes
-    prime_numbers = find_primes(start, end)
-
-    print("Прості числа у заданому діапазоні:", prime_numbers)
+            print("Прості числа у заданому діапазоні:", i)
