e9700ecc19dea6e8aa4ff8ecdab9de388067b7bd
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-21 17:05:22 +0300

    add

--- main.py
+++ main.py
@@ -5,18 +5,35 @@
 # Наприклад, три — це просте число, а чотири — ні.
 
 
-x = int(input("Введіть початкове число діапазону: "))
-y = int(input("Введіть кінцеве число діапазону: "))
+# start = int(input("Введіть початкове число діапазону: "))
+# end = int(input("Введіть кінцеве число діапазону: "))
+#
+# for number in range(start, end + 1):
+#
+#     if number / number == 1 and number % 2:   # створюємо умову для простого числа
+#         print(number, "- просте число")       # якщо умова виконується це є просте число
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
 
-for num in range(x, y + 1):
-    if is_prime(num):
-        print(num)
+# # ЗАВДАННЯ 2
+#
+#  #Вивести на екран таблицю множення для всіх чисел від 1 до 10.
+#
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+#
+# for x in range(1, 11):            # 1-ий множник
+#     for y in range(1, 11):        # 2-ий множник
+#         result = x * y
+#         print(f"{x} x {y} = {result}")
 
+
+# # ЗАВДАННЯ 3
+#
+# # Вивести на екран таблицю множення в діапазоні, зазначеному користувачем.
+#
+# numOne = int(input("Введіть початкове число : "))
+# numTwo = int(input("Введіть кінцеве число : "))
+#
+# for x in range(numOne, numTwo + 1):        # створюємо цикл для 1-го множника
+#     for y in range(1, 11):                 # створюємо цикл для 2-го множника
+#         result = x * y
+#         print(f"{x} x {y} = {result}")
